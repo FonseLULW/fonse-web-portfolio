4e798319912cfde04cc2e995c5199a05d6c8933c
FonseLULW <jalfonsclarito@gmail.com>
2023-05-11 19:11:13 -0700

    Added info to contactme.tsx

--- src/panels/contactme.tsx
+++ src/panels/contactme.tsx
@@ -1,9 +1,28 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Flex, Text, Image, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, Textarea, Divider, Button, VStack, Link, Box } from '@chakra-ui/react'
+import jslogo from '../assets/js-test.png'
 
 export default function ContactMePanel() {
     return (
-        <Container bg='#6096B4' maxW='none'>
-            ContactMePanel
+        <Container maxW='none'>
+            <Text fontSize='2xl'>Contact Me</Text>
+            <VStack>
+                <Flex>{/* Decompose into smaller component */}
+                    <Box><Image boxSize={50} src={jslogo} alt='JavaScript'/></Box>
+                    <Link>jalfonsclarito@gmail.com</Link>
+                </Flex>
+                <Flex>{/* Decompose into smaller component */}
+                    <Box><Image boxSize={50} src={jslogo} alt='JavaScript' /></Box>
+                    <Link>jalfonsclarito@gmail.com</Link>
+                </Flex>
+            </VStack>
+            <Divider />
+            <Text fontSize='md'>or send me a message directly...</Text>
+            <FormControl>
+                <Input type="text" placeholder='Name'/>
+                <Input type="email" placeholder='Email'/>
+                <Textarea placeholder="Message"></Textarea>
+                <Button type="submit">Send</Button>
+            </FormControl>
         </Container>
     )
 }
