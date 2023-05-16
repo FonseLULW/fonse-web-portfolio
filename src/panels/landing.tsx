a9415c112100ef69d7296d0906db3cf4b5b9edaf
FonseLULW <jalfonsclarito@gmail.com>
2023-05-16 15:45:57 -0700

    Styled landing

--- src/panels/landing.tsx
+++ src/panels/landing.tsx
@@ -1,34 +1,38 @@
-import { Container, Text, Image, Button, Flex, VStack, Box, Center } from '@chakra-ui/react'
+import { Container, Text, Image, Button, Flex, VStack, Box, Center, Grid, GridItem } from '@chakra-ui/react'
 import avatar from '../assets/avatar.jpg'
 
 export default function LandingPanel() {
-    let [titleHeight, descHeight, imgHeight, btnHeight] = ['20%', '15%', '40%', '25%'];
     return (
-        <Container maxW='none' padding='0.5em'>
-            <VStack h='100%' spacing='0px'>
-                <Flex height={titleHeight} direction='column' justifyContent='flex-end'>
-                    <Text fontSize='4xl' align='center'>Jose Alfonso Clarito</Text>
-                    <Text fontSize='2xl' align='center'>Software Developer</Text>
-                </Flex>
-                <Flex height={descHeight} direction='column' justifyContent='flex-end' alignItems='center'>
-                    <Text align='center' fontSize='xl' width='80%'>Lorem ipsum dolor sit amet consectetur.</Text>
-                </Flex>
-                <Container height={imgHeight}>
-                    <Center h='100%' w='100%'>
-                        <Box boxSize='2xs'>
-                            <Image
-                                borderRadius='full'
-                                src={avatar}
-                                alt='Fonse Clarito'
-                                align='center'
-                            />
-                        </Box>
-                    </Center>
-                </Container>
-                <Container height={btnHeight} centerContent>
-                    <Button variant='solid' size='lg' width='100%'>Get in Touch</Button>
-                </Container>
-            </VStack>
-        </Container>
+        <Grid maxW='none' padding='0.5em' templateAreas={`
+            "avatar name"
+            "avatar role"
+            "avatar summary"
+            "avatar action"
+        `}>
+            <GridItem area='name'>
+                <Text fontSize='4xl' align='center'>Jose Alfonso Clarito</Text>
+            </GridItem>
+            <GridItem area='role'>
+                <Text fontSize='2xl' align='center'>Software Developer</Text>
+            </GridItem>
+            <GridItem area='summary'>
+                <Text align='center' fontSize='xl'>Lorem ipsum dolor sit amet consectetur.</Text>
+            </GridItem>
+            <GridItem rowSpan={4} area='avatar'>
+                <Center h='100%' w='100%'>
+                    <Box boxSize='2xs'>
+                        <Image
+                            borderRadius='full'
+                            src={avatar}
+                            alt='Fonse Clarito'
+                            align='center'
+                        />
+                    </Box>
+                </Center>
+            </GridItem>
+            <GridItem area='action'>
+                <Button variant='solid' size='lg' width='100%'>Get in Touch</Button>
+            </GridItem>
+        </Grid>
     )
 }
