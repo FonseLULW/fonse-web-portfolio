f81271b0cf98eece52af1e1c499338b90096db75
FonseLULW <jalfonsclarito@gmail.com>
2023-07-21 17:23:34 -0700

    Added mobile compatible layout to landing panel

--- src/panels/landing.tsx
+++ src/panels/landing.tsx
@@ -21,28 +21,28 @@ export default function LandingPanel() {
             `
         ]} gridTemplateColumns={[null, null, '64fr 36fr']} gridTemplateRows={[null, null, '40fr auto auto 20fr 40fr']}>
             <GridItem area='hello' alignSelf={'flex-end'}>
-                <Text fontSize='4xl' align='center'>Hello, I am</Text> 
+                <Text fontSize={['2xl', '3xl', '4xl']} align='center'>Hello, I am</Text> 
             </GridItem>
             <GridItem area='name'>
-                <Heading as='h1' fontSize='6xl' textAlign={'center'} fontWeight={'semibold'} fontFamily={'name'} textTransform='uppercase' letterSpacing='widest'>Jose Alfonso Clarito</Heading>
+                <Heading as='h1' fontSize={['4xl', '5xl', '6xl']} textAlign={'center'} fontWeight={'semibold'} fontFamily={'name'} textTransform='uppercase' letterSpacing='widest'>Jose Alfonso Clarito</Heading>
             </GridItem>
             <GridItem area='role'>
                 {/* Replace with fancy animation */}
-                <Text fontSize='4xl' align='center'><Highlight query='Software Developer' styles={{ px: '2', py: '1', rounded: 'full', bg: 'primary_variant' }}>Software Developer</Highlight></Text> 
+                <Text fontSize={['2xl', '3xl', '4xl']} align='center'><Highlight query='Software Developer' styles={{ px: '2', py: '1', rounded: 'full', bg: 'primary_variant' }}>Software Developer</Highlight></Text> 
             </GridItem>
             <GridItem area='summary' alignSelf={'end'}>
-                <Text mb={'0.25em'} align='center' fontSize='3xl'>Lorem ipsum dolor sit amet consectetur.</Text>
+                <Text mb={'0.25em'} align='center' fontSize={['xl', '2xl', '3xl']}>Lorem ipsum dolor sit amet consectetur.</Text>
             </GridItem>
             <GridItem area='avatar'>
-                <Center h='100%' w='100%' bg='primary' borderBottomLeftRadius={'35%'} borderTopRightRadius={['35%', null, '0%']}>
+                <Center h='100%' w='100%' bg={['initial', 'secondary', 'primary']} borderRadius={['0%', '0%', '0% 0% 0% 35%']}>
                     <Box justifySelf={'center'}>
                         <Image
                             margin={'auto'}
-                            boxSize={'75%'}
+                            boxSize={['75%', '50%','75%']}
                             borderRadius={['50%', null, '25%']}
-                            borderColor={'black'}
+                            borderColor={'primary_variant'}
                             borderStyle={'solid'}
-                            borderWidth={1}
+                            borderWidth={10}
                             src={avatar}
                             alt='Fonse Clarito'
                             align='center'
@@ -52,7 +52,7 @@ export default function LandingPanel() {
                 </Center>
             </GridItem>
             <GridItem area='action'>
-                <Center mt={'0.25em'}><Button variant='solid' size='lg' fontSize={'3xl'}>Get in Touch</Button></Center>
+                <Center mt={'0.25em'}><Button variant='solid' size={'lg'} fontSize={['2xl', '2xl', '3xl']}>Get in Touch</Button></Center>
             </GridItem>
         </Grid>
     )
